8264dbca6357214744969b81b02742aa2b5f90d2
Dimitar Bounov <d1m0@protonmail.com>
2025-10-20 18:42:32 +0300

    nit

--- src/debug/decoding/index.ts
+++ src/debug/decoding/index.ts
@@ -5,4 +5,11 @@ export * from "./stack";
 export * from "./storage";
 export * from "./value";
 export * from "./view";
-export { isPointerView, inRange, getContractLayoutType } from "./utils";
+export {
+    isPointerView,
+    isArrayLikeView,
+    isStructView,
+    isIndexableView,
+    inRange,
+    getContractLayoutType
+} from "./utils";
